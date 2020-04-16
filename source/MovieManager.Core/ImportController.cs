108e87478e40b2f8f79774c827a05bb59d9be64a
Pero1709 <32167797+Pero1709@users.noreply.github.com>
2020-04-16 18:40:57 +0200

    Update ImportController.cs

--- source/MovieManager.Core/ImportController.cs
+++ source/MovieManager.Core/ImportController.cs
@@ -13,7 +13,37 @@ namespace MovieManager.Core
         /// </summary>
         public static IEnumerable<Movie> ReadFromCsv()
         {
-            throw new NotImplementedException();
+            string filePath = MyFile.GetFullFolderNameInApplicationTree(Filename);
+            string[] lines = File.ReadAllLines(filePath);
+            IDictionary<string, Category> categories = new Dictionary<string, Category>();
+            IList<Movie> movies = new List<Movie>();
+
+            foreach (var item in lines)
+            {
+                string[] parts = item.Split(';');
+                string title = parts[0];
+                int year = Convert.ToInt32(parts[1]);
+                string[] catergoryName = new string[] { "Action", "Adventure", "Animation", "Biography", "Comedy", "Crime","Documentary","Drama","Fantasy","Horror","Thriller"};
+                
+                int duration = Convert.ToInt32(parts[3]);
+                Category category;
+                Movie movie = new Movie
+                {
+                    Title = title,
+                    Year = year,
+                    Duration = duration
+                };
+
+                Category action;
+                if(categories.TryGetValue(catergoryName[1],out action))
+                {
+
+                }
+
+                
+              
+            }
+            return movies;
         }
 
     }
